91bb21cfeef1351051407fcb990aac9fe4695482
Spínola <teu_almeida20010@hotmail.com>
2022-03-21 18:02:46 -0300

    Começando o bot para celular Ju

---
+++ excel.py
@@ -0,0 +1,25 @@
+import pyautogui as ptgui
+import time
+
+
+ptgui.alert("O programa vai começar")
+
+i = 0
+
+
+ptgui.hotkey('win','8')
+
+ptgui.PAUSE = .5
+
+# while(i < 2):
+ptgui.hotkey("ctrl", "1")
+ptgui.click(1085, 81)
+ptgui.click(1025, 259)
+ptgui.click(937, 335)
+ptgui.click(937, 415)
+ptgui.click(1086, 587)
+j = i
+
+while(j < i + 5):         
+    
+    # i += 1

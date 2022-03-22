14ee1ccd7a1f7eb7789ebc9dd00c386bc4599e63
Spínola <teu_almeida20010@hotmail.com>
2022-03-22 18:34:40 -0300

    Bot whats quase pronto. Falta bem pouco.

--- excel.py
+++ excel.py
@@ -1,25 +1,43 @@
+from turtle import position
 import pyautogui as ptgui
 import time
 
-
-ptgui.alert("O programa vai começar")
+resp = int(input("Qual opção você quer? "))
+pos = 100 + (50 * resp)
 
 i = 0
+j = 1
 
-
-ptgui.hotkey('win','8')
+ptgui.hotkey('win', '8')
 
 ptgui.PAUSE = .5
 
-# while(i < 2):
-ptgui.hotkey("ctrl", "1")
-ptgui.click(1085, 81)
-ptgui.click(1025, 259)
-ptgui.click(937, 335)
-ptgui.click(937, 415)
-ptgui.click(1086, 587)
-j = i
+while(i < 3):
+    ptgui.hotkey("ctrl", "1")
+    ptgui.click(1085, 81)
+    ptgui.click(1025, pos)
+    ptgui.scroll(-100)
+    ptgui.click(530, 173)
+    ptgui.click(530, 369)
+    ptgui.click(1086, 587)
 
-while(j < i + 5):         
+    l = j
+    while j < l + 5:
+        for t in range(j):
+            ptgui.press("down")
+        ptgui.press("enter")
+        
+        ptgui.moveTo(512,280)
+        ptgui.scroll(500 * j)
+        ptgui.click(512, 280)
+        
+        j += 1
+
+    ptgui.click(782, 520)
     
-    # i += 1
+    i += 1
+
+# time.sleep(3)
+# print(ptgui.position())
+# time.sleep(3)
+# print(ptgui.position())

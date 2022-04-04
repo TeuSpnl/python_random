fdb8353713d289527ec3a598becee574d6313b1a
Spínola <teu_almeida20010@hotmail.com>
2022-04-04 16:28:49 -0300

    Adicionando bolinha no pong

--- kivy_tests/pong/main.py
+++ kivy_tests/pong/main.py
@@ -1,16 +1,28 @@
 from kivy.app import App
 from kivy.uix.widget import Widget
-from kivy.uix.label import Label
-from kivy.lang import Builder
+from kivy.properties import NumericProperty, ReferenceListProperty
+from kivy.vector import Vector
 
-GUI = Builder.load_file("layout.kv")
+class PongBall(Widget):
+    velocity_x = NumericProperty(0)
+    velocity_y = NumericProperty(0)
+    
+    velocity = ReferenceListProperty(velocity_x, velocity_y)
+    
+    def move(self):
+        self.pos = Vector(*self.velocity) + self.pos
+        
+    Clock.shedule_interval(game.update, 1.0/60.0)
+
 
 class PongGame(Widget):
     pass
 
+
 class PongApp(App):
     def build(self):
         return PongGame()
+
 
 if __name__ == '__main__':
     PongApp().run()

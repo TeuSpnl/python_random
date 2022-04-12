a4f64be359467ca2ffddd368329b15e9b019e006
Spínola <teu_almeida20010@hotmail.com>
2022-04-12 18:28:45 -0300

    Pequenas alterações

--- kivy_tests/Kivy_TheLab/main.py
+++ kivy_tests/Kivy_TheLab/main.py
@@ -3,6 +3,7 @@ from kivy.uix.button import Button
 from kivy.uix.widget import Widget
 from kivy.app import App
 from kivy.uix.boxlayout import BoxLayout
+from kivy.uix.scrollview import ScrollView
 
 class BoxLayoutExample(BoxLayout):
     # def __init__(self, **kwargs):
@@ -12,6 +13,9 @@ class BoxLayoutExample(BoxLayout):
     #     b2 = Button(text = "B")
     #     self.add_widget(b1)
     #     self.add_widget(b2)
+    pass
+
+class ScrollViewEx(ScrollView):
     pass
 
 class MainWidget(Widget):

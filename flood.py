3f78e3c15f95c58d618df67e6e84ed15c6bf1f9b
Spínola <teu_almeida20010@hotmail.com>
2022-06-30 19:55:28 -0300

    pequena smudanças flood

--- flood.py
+++ flood.py
@@ -1,13 +1,18 @@
 import pyautogui as ptgui
 import time
 
-ptgui.hotkey('win', '5')
+ptgui.hotkey('win', '3')
 
 i = 0
 
 time.pause = 5
 
-while (i < 500):
-    ptgui.hotkey("ctrl", "v")
+while (i < 150):
+    ptgui.press("f2")
+    ptgui.press("end")
+    ptgui.press("backspace")
+    ptgui.press("v")
     ptgui.press("enter")
+    ptgui.press("enter")
+    ptgui.press("down")
     i += 1

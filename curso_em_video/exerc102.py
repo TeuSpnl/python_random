a7f4ec2edc19de38e8682872e11d43ec2208b959
Spínola <teu_almeida20010@hotmail.com>
2022-07-12 18:40:47 -0300

    organizando o 102

--- curso_em_video/exerc102.py
+++ curso_em_video/exerc102.py
@@ -1,29 +1,28 @@
 def fatorial(valor, show=False):
     """
     Calcula o fatorial de um número e mostra, caso o usuário deseje, a equação.
-    
+
     :param valor: O número a ser fatorado.
     :param show: (opcional) Mostra ou não a equação
-    
+
     :return: O resultado do fatorial do valor.
     """
-    
-    
+
     i = valor
     result = 1
     while i > 0:
         if show:
-            print(i, end= ' ')
+            print(i, end=' ')
             if i != 1:
                 print('x', end=' ')
             else:
                 print('=', end=' ')
+
+        result *= i
         i -= 1
-        
-        result *= valor
-        valor -= 1
-        
+
     return f"{result}"
-        
+
+
 print('-' * 20)
-print(fatorial(5, show= True))
+print(fatorial(5, show=True))

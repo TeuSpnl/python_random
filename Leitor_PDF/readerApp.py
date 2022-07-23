c5dbe4ace3d78fad377b7bec40c50bae0ad521d9
Spínola <teu_almeida20010@hotmail.com>
2022-07-22 23:34:47 -0300

    Colocando a lógica certa e tentativa frustada de criar uma tela

--- Leitor_PDF/readerApp.py
+++ Leitor_PDF/readerApp.py
@@ -1,6 +1,5 @@
-from time import sleep
 import timeit
-
+from time import sleep
 
 lista = ["hoje", "é", "um", "novo", "dia", "hoje", "é", "um", "novo", "dia", "hoje", "é", "um", "novo", "dia", "hoje", "é", "um", "novo", "dia", "hoje", "é", "um", "novo", "dia", "hoje", "é", "um", "novo", "dia", "hoje", "é", "um", "novo", "dia", "hoje", "é", "um", "novo", "dia", "hoje", "é", "um", "novo", "dia", "hoje", "é", "um", "novo", "dia", "hoje", "é", "um", "novo", "dia", "hoje", "é", "um", "novo", "dia", "hoje", "é", "um", "novo", "dia", "hoje", "é", "um", "novo", "dia", "hoje", "é", "um", "novo", "dia", "hoje", "é", "um", "novo", "dia", "hoje", "é", "um", "novo", "dia", "hoje", "é", "um", "novo", "dia", "hoje", "é", "um", "novo", "dia", "hoje", "é", "um", "novo", "dia", "hoje", "é", "um", "novo", "dia", "hoje", "é", "um", "novo", "dia", "hoje", "é", "um", "novo", "dia", "hoje", "é", "um", "novo", "dia", "hoje", "é", "um", "novo", "dia", "hoje", "é", "um", "novo", "dia", "hoje", "é", "um", "novo", "dia", "hoje", "é", "um", "novo", "dia", "hoje", "é", "um", "novo", "dia", "hoje", "é", "um", "novo", "dia", "hoje", "é", "um", "novo", "dia", "hoje", "é", "um", "novo", "dia", "hoje", "é", "um", "novo", "dia", "hoje", "é", "um", "novo", "dia", "hoje", "é", "um", "novo", "dia", "hoje", "é", "um", "novo", "dia", "hoje", "é", "um", "novo", "dia", "hoje", "é", "um", "novo", "dia", "hoje", "é", "um", "novo", "dia", "hoje", "é", "um", "novo", "dia"]
 
@@ -9,7 +8,7 @@ lista = ["hoje", "é", "um", "novo", "dia", "hoje", "é", "um", "novo", "dia", "
 inicio = timeit.default_timer()
 for palavra in lista:
     print(palavra)
-    sleep(.3)
+    sleep(60/300)
 fim = timeit.default_timer()
 
 print(f"Tempo de execução: {fim - inicio}")
